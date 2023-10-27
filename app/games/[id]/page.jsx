2d922d54bf8340823e4e8aa124d5aff827c6efc7
isakamitech <isabelasouzade.564@gmail.com>
2023-10-27 08:20:26 -0300

    alteracoes

--- app/games/[id]/page.jsx
+++ app/games/[id]/page.jsx
@@ -3,8 +3,7 @@ import { fetchApiDetails } from '@/data/apiconsumer';
 import React, { useState, useEffect } from 'react';
 import styles from './page.module.css';
 import Header from '@/app/components/header/header';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+
 
 const gameDescription = ({ params }) => {
     const [games, setGames] = useState([]);
@@ -24,49 +23,43 @@ const gameDescription = ({ params }) => {
     return (
         <div className={styles.container}>
             <Header />
-            <div className={styles.maincontainer}>
-            <h1>{games.name}</h1>
-            <img className={styles.gameThumb} src={games.background_image} alt={games.name} />
-            <p className={styles.rating}>{games.rating}</p>
-            <p className={styles.released}>{games.released}</p>
-            <p className={styles.description}>{games.description_raw}</p>
-            <div className={styles.tabs}>
-            <Tabs>
-    <TabList>
-      <Tab>Plataformas</Tab>
-      <Tab>Gêneros</Tab>
-    </TabList>
+            <div className={styles.imageContainer}>
+                <img className={styles.gameThumb} src={games.background_image} alt={games.name} height={540} width={1200} />
+                <h1 className={styles.h1}>{games.name}</h1>
 
-    <TabPanel>
-      <h2>{
-                games.parent_platforms ? (
-                    games.parent_platforms.map((platform) => {
-                        return (
+                <div className={styles.divrating}>
+                    <p className={styles.rating}>{games.rating}</p>
+                </div>
+
+                <div className={styles.platformsContainer}>
+                    {games.parent_platforms ? (
+                        games.parent_platforms.map((platform) => (
                             <p className={styles.platforms}>{platform.platform.name}</p>
-                        )
-                    })
-                ) : (
-                    null
-                )
-            }</h2>
-    </TabPanel>
-    <TabPanel>
-      <h2>{
-                games.genres ? (
-                    games.genres.map((genre) => {
-                        return (
-                            <p className={styles.genres}>{genre.name}</p>
-                        )
-                    })
-                ) : (
-                    null
-                )
-            }</h2>
-    </TabPanel>
-  </Tabs>
+                        ))
+                    ) : null}
+                </div>
             </div>
-            
+
+            <h3 className={styles.h3}>Data de lançamento</h3>
+            <h6 className={styles.released}>{games.released}</h6>
+
+
+
+            <div className={styles.descricao}>
+                <p className={styles.h2}>{games.name} Game</p>
+                <p className={styles.description}>{games.description_raw}</p>
             </div>
+
+            <h6 className={styles.h6}>Genero do jogo</h6>
+            {games.genres ? (
+                games.genres.map((genre) => (
+
+                    <div className={styles.genrediv}>
+                        <p className={styles.genres}>{genre.name}</p>
+                    </div>
+                ))
+            ) : null}
+
         </div>
     );
 };
