b0526a00cbd6949da6f38acbe966aedcbf4bd927
isakamitech <isabelasouzade.564@gmail.com>
2023-11-03 13:09:08 -0300

    feat: ultimo commit

--- data/gamedata.js
+++ data/gamedata.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const fetchAsyncGames = async (page = 1) => {
-    const URL_API = `https://api.rawg.io/api/games?key=219af9c69fd8413cb6d8e15b6471d74a&page=${page}`;
+    const URL_API = `https://api.rawg.io/api/games?key=da3625204a87477ca22170f1f6d85543&page=${page}`;
     const { data } = await axios.get(URL_API);
     return data;
   };
