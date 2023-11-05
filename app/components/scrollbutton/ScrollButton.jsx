1b2da1e5588943fb13beb9990217eb153f294f71
Arthur Borges <123407087+Arthursenai@users.noreply.github.com>
2023-11-05 20:15:43 -0300

    feat: try to fix window problem

--- app/components/scrollbutton/ScrollButton.jsx
+++ app/components/scrollbutton/ScrollButton.jsx
@@ -1,18 +1,33 @@
-"use client";
 import { useEffect, useState } from 'react';
 import { IoIosArrowUp, IoIosArrowDown } from 'react-icons/io';
 import styles from '@/app/page.module.css';
 
 const ScrollButton = () => {
-    return window.scrollY > 200 ? (
-        <div className={styles.scrollbtn} onClick={() => window.scrollTo(0, 0)}>
-          <IoIosArrowUp className={styles.icons} />
-        </div>
-      ) : (
-        <div className={styles.scrollbtn} onClick={() => window.scrollTo(0, 100000)}>
-          <IoIosArrowDown className={styles.icons} />
-        </div>
-      )
+  const [scrollY, setScrollY] = useState(0);
+
+  // Add an event listener to update the scrollY state
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return scrollY > 200 ? (
+    <div className={styles.scrollbtn} onClick={() => window.scrollTo(0, 0)}>
+      <IoIosArrowUp className={styles.icons} />
+    </div>
+  ) : (
+    <div className={styles.scrollbtn} onClick={() => window.scrollTo(0, 100000)}>
+      <IoIosArrowDown className={styles.icons} />
+    </div>
+  );
 }
 
 export default ScrollButton;
